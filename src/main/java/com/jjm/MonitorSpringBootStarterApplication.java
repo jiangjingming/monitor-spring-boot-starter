d12b74058e7e2b9a5112eb955a3f612451ac6451
jiangjingming <baili@maihaoche.com>
2017-10-26 12:59:15 +0800

    项目初始化

---
+++ src/main/java/com/jjm/MonitorSpringBootStarterApplication.java
@@ -0,0 +1,12 @@
+package com.jjm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorSpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorSpringBootStarterApplication.class, args);
+	}
+}
